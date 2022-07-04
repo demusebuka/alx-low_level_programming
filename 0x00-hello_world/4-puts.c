62d224df456f0c1c446c6f333f1c7994bcf971f1
demusebuka <demusebuka@gmail.com>
2022-07-04 05:09:40 -0700

    puts c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
-/
+#include <string.h>
 
- * main - Entry point
- *
- *  * Return: Always 0 (sucess)
- *
- *   */
-
+/**
+ * main - Entry point*
+ * Return: Always 0 (sucess)
+ */
  int main(void)
 
 {
+char str[]="\"Programming is like building a multilingual puzzle";
 
-	puts("\"Programming is like building a multilingual puzzle");
+puts(str);
 
-	return (0);
-
+return (0);
 }
