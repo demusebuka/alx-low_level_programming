403b67b074a74145426a86aca666f21e75a3350a
demusebuka <demusebuka@gmail.com>
2022-07-04 06:20:05 -0700

    size c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,10 @@
 #include <stdio.h>                                     
 
 /**                                                    
- * main - Entry point                                   
- * printing the size of various types                   
- * Return: Always 0 (success)    
+ *main - Entry point                                   
+ *printing the size of various types                   
+ *Return: Always 0 (success)    
  */                                                   
-
 int main(void)                                         
 {                                                      
 int i;                                                 
