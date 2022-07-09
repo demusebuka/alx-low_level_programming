be0df55ef5ce41aa97faa80ff6c229bd3b9873c3
demusebuka <demusebuka@gmail.com>
2022-07-09 15:30:04 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,22 +1,23 @@
 #include <stdio.h> 
-
 #include "main.h" 
-
-/**
- * _isupper - checks for uppercase letters
- *
- * @c: parametr to be printed
- *
- * return: 0 or 1 depending on the condition
- */
-
-int_isupper(int c)
-{
-	if(c >= 'A' && c <= 'Z')
-
-	return (1);
-
-	else
-
- return (0); 
+ 
+/** 
+ * _isupper - checks for uppercase letters 
+ * 
+ * @c: parametr to be printed 
+ * 
+ * Return: 0 or 1 depending on the condition 
+ */ 
+ 
+ 
+ 
+int _isupper(int c) 
+{ 
+ if (c >= 'A' && c <= 'Z') 
+ 
+  return (1); 
+ 
+ else 
+ 
+  return (0); 
 }
