9f731dfac71cc954c84fa7ee10ae1f94311b0310
demusebuka <demusebuka@gmail.com>
2022-07-09 14:00:49 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include <stdio.h> 
+
+#include "main.h" 
+
+/**
+ * _isupper - checks for uppercase letters
+ *
+ * @c: parametr to be printed
+ *
+ * return: 0 or 1 depending on the condition
+ */
+
+int_isupper(int c)
+{
+	if(c >= 'A' && c <= 'Z')
+
+	return (1)
+
+	else
+
+ return (0); 
+}
