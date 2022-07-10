775ad86968be54515e94ad2cf698bcbe8246617d
demusebuka <demusebuka@gmail.com>
2022-07-10 07:13:02 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,41 @@
+#include "main.h" 
+ 
+/** 
+ * mul - multiplies two integer numbers 
+ * @a: first integer 
+ * @b: second integer 
+ * Return: sum 
+ */ 
+ 
+ 
+ 
+int mul(int a, int b) 
+{ 
+ int sum = a * b; 
+ 
+ 
+ 
+ return (sum); 
+ 
+} 
+ 
+ 
+3-print_numbers.c 
+ 
+#include "main.h" 
+ 
+/** 
+ * print_numbers -  checks for checks for a digit (0 through 9). 
+ * 
+ * Return: Always 0. 
+ */ 
+void print_numbers(void) 
+{ 
+ int a; 
+ 
+ for (a = 48; a < 58; a++) 
+ { 
+ _putchar(a); 
+ } 
+ _putchar('\n'); 
+} 
