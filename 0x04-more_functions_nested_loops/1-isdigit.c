e7c8d5f5714a8597341c16eb913bb35f02a7b539
demusebuka <demusebuka@gmail.com>
2022-07-10 08:26:23 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,34 +1,33 @@
+#include "main.h"
 
-#include "main.h" 
- 
-#include <ctype.h> 
- 
- 
- 
-/** 
- * _isdigit - check for numbers 
- * 
- * @c: parameter to print 
- * 
- * Return: 0 or 1 
- */ 
- 
- 
- 
-int _isdigit(int c) 
-{ 
- if (c >= '0' && c <= '9') 
- { 
- 
-  return (1); 
- 
- } 
- 
- else 
- { 
- 
-  return (0); 
- 
- } 
- 
+#include <ctype.h>
+
+
+
+/**
+ * _isdigit - check for numbers
+ *
+ * @c: parameter to print
+ *
+ * Return: 0 or 1
+ */
+
+
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+
+		return (1);
+
+	}
+
+	else
+	{
+
+		return (0);
+
+	}
+
 }
