dbacd11768c08061c88c1217a774ec8d9ab7dd00
demusebuka <demusebuka@gmail.com>
2022-07-10 09:46:12 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,13 +5,18 @@
  *
  * Return: 0
  */
-void print_alphabet(void)
-{
-        char c;
 
-        for (c = 'a'; c <= 'z'; c++)
+void print_alphabet(void)
+
+{
+
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
-		_putchar(c);					
+		_putchar(alphabet);
 	}
-_putchar('\n');
+
+	_putchar('\n');
+
 }
