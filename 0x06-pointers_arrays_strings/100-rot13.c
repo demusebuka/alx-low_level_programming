d96103108151c6e563ee27a98571e6554f12f4c7
demusebuka <demusebuka@gmail.com>
2022-07-14 12:02:37 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+
+
+/**
+ * *rot13 - a function that encodes a string using rot13
+ *
+ * @c: string to be utilised
+ *
+ * Return: encoded string.
+ */
+char *rot13(char *c)
+{
+	int a = 0;
+
+	int b = 0;
+
+	char c_alph[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+	char c_rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+
+
+	for (a = 0; c[a] != '\0'; a++)
+	{
+
+		for (b = 0; c_alph[b] != '\0'; b++)
+		{
+
+			if (c[a] == c_alph[b])
+			{
+
+				c[a] = c_rot13[b];
+
+				break;
+
+			}
+
+		}
+
+	}
+
+
+
+	return (c);
+}
