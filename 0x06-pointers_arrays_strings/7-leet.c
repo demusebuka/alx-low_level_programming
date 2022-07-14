fd1c4f1d4325ab18518d884c152bb0d0e2659311
demusebuka <demusebuka@gmail.com>
2022-07-14 12:00:36 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * *leet - writes a function that encodes a string to 1337
+ * @c: string to encode
+ * Return: encoded string
+ */
+char *leet(char *c)
+{
+
+	int a = 0;
+
+	int b = 0;
+
+	char c_low[] = {'a', 'e', 'l', 'o', 't'};
+
+	char c_up[] = {'A', 'E', 'L', 'O', 'T'};
+
+	char c_num[] = {'4', '3', '1', '0', '7'};
+
+
+
+	while (c[a] != '\0')
+	{
+
+		for (b = 0; b < 5; b++)
+		{
+			if (c[a] == c_low[b] || c[a] == c_up[b])
+
+				c[a] = c_num[b];
+
+		}
+
+		a++;
+
+	}
+
+
+
+	return (c);
+}
