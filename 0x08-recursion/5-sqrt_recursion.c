cbbb1f675010248fa7099c51e23d845c9ead6c41
demusebuka <demusebuka@gmail.com>
2022-07-21 06:35:35 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * isqrt - Evaluate from 1 to n
+ *
+ * @x: input integer
+ *
+ * @y: iterated integer
+ *
+ * Return: square root or -1
+ */
+int isqrt(int x, int y)
+{
+	if ((y * y) == x)
+
+		return (y);
+
+	else if ((y * y) > x)
+
+		return (-1);
+
+	return (isqrt(x, y + 1));
+
+}
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ *
+ * @n: integer
+ *
+ * Return: natural square root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+
+		return (-1);
+
+	return (isqrt(n, 0));
+
+}
