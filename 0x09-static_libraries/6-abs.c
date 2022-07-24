8bbb84fecf33d8ec51ce143bda24cf42a1268d98
demusebuka <demusebuka@gmail.com>
2022-07-24 12:20:38 -0700

    task 0

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - absolute integer
+ * @n: para
+ * Description: blabla
+ * Return: 0
+ */
+int _abs(int n)
+{
+	int calc = abs(n);
+
+	return (calc);
+}
